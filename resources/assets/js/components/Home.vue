8388883abfdc38e4966009435fe6358c570720eb
Avinash-kumar-keshri <avinash.keshri@cssjockey.com>
2018-08-29 17:46:29 +0530

    Initial Commit

---
+++ resources/assets/js/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="panel">
+        <p class="panel-heading">
+            Vuejs Phonebook
+            <button class="button is-primary is-outlined">
+                Add New
+            </button>
+        </p>
+
+        <div class="panel-block">
+
+        </div>
+
+        <a class="panel-block">
+            <span class="column is-9">
+              marksheet
+            </span>
+
+            <span class="panel-icon column is-1">
+              <i class="has-text-danger fa fa-trash" aria-hidden="true"></i>
+            </span>
+
+            <span class="panel-icon column is-1">
+              <i class="has-text-info fa fa-edit" aria-hidden="true"></i>
+            </span>
+
+            <span class="panel-icon column is-1">
+              <i class="has-text-primary fa fa-eye" aria-hidden="true"></i>
+            </span>
+        </a>
+
+
+    </nav>
+</template>
---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                <strong>Vuejs Phonebook App</strong> by <a href="https://github.com/Avinash-kumar-keshri">Avinash kumar keshri</a>.Access The source code
+                <a href="https://github.com/Avinash-kumar-keshri/phonebook">Github.com/Phonebook</a>
+
+            </p>
+        </div>
+    </footer>
+</template>
